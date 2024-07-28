08d367eeaded12d0032586ba4039ab5c8769436b
Juan2850 <castaneda2850@gmail.com>
2024-07-28 10:57:46 -0500

    Creación de la entidad Topical
    Creación de una nueva entidad Topical con sus atributos que guardan en la bases de datos

---
+++ src/main/java/com/foro/foro_alura/entities/Topical.java
@@ -0,0 +1,28 @@
+package com.foro.foro_alura.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "topics")
+public class Topical {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String message;
+    @Column(name = "date_of_creation")
+    private LocalDate dateOfCreation;
+    @Column(name = "topical_status")
+    private boolean topicalStatus;
+    private String author;
+    private String course;
+
+}
